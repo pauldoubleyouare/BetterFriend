ef2a632ae5c68e5206851d35d189bc40e5db88ad
pauldoubleyouare <paulrosenberg13@gmail.com>
2018-09-01 17:58:48 -0700

    updated initial deploy

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+'use strict';
+
 const express = require('express');
+
 const app = express();
-app.use(express.static('public'));
-app.listen(process.env.port || 8080);
+
+app.use(express.static('public')); //this is serving the static files in 'public'
+
+//require is a function, and 'main' is a property on the require function
+if (require.main === module) {
+    app.listen(process.env.port || 8080, function() {
+        console.info(`BetterFriend is listening on ${this.address().port}`);
+    });
+}
+
+module.exports = app;
---
+++ test/test-server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+//initial test to check if the home page is being displayed
+describe("Home page", function() {
+    it("should display the home page", function() {
+        return chai
+            .request(app)
+            .get("/")
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                // console.log(res);
+            });
+    });
+});
