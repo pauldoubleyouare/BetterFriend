93102b66b00bb26909340465e946e2e37bd6e3fe
pauldoubleyouare <paulrosenberg13@gmail.com>
2018-09-05 17:04:32 -0700

    updated server.js for heroku initial deploy

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const app = express();
 app.use(express.static('public')); //this is serving the static files in 'public'
 
 //require is a function, and 'main' is a property on the require function
+// checking to see if we're in a node.js environment - if we are, then open port
 if (require.main === module) {
     app.listen(process.env.port || 8080, function() {
-        console.info(`BetterFriend is listening on ${this.address().port}`);
+        console.info(`app is listening on ${this.address().port}`);
     });
 }
+
 
 module.exports = app;
