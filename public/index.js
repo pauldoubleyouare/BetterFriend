e0ac3c8f89d983a102516df2ca2c04cf08ccea2c
pauldoubleyouare <paulrosenberg13@gmail.com>
2018-09-08 18:04:44 -0700

    updated client side

--- public/index.js
+++ public/index.js
@@ -1,43 +1,169 @@
-function loginButton() {
-	// on click, the app starts and you're taken to the Dashboard page
-	//
-	$(".loginButton").on("click", function() {
-		$("#loginPage").hide();
-		$("#dashboardPage").show();
+function renderHomePage() {
+	$("main").html(`
+	<div class="page home">
+	<h1>Home</h1>
+		<button class="btn login">Login</button>
+		<button class="btn createBfAccount">Create Account</button>
+	</div>`);
+	$(".btn.login").on("click", function(){
+		renderLoginPage();
+	});
+	$(".btn.createBfAccount").on("click", function() {
+		renderCreateAccountPage();
+	})
+
+}
+
+
+function renderLoginPage() {
+	$("main").html(`
+	<div class="page login">
+		<h1>Login</h1>
+		<button class="btn" id="createBfAccount">Create Account</button><br>
+		<form>
+			Username:<br>
+			<input type="text" class="existingUsername" required>
+			<br>
+			Password:<br>
+			<input type="password" name="password" required><br>
+			<input type="submit" class="btn submitUsrPas">
+			<input type="reset" class="btn resetLogin">
+		</form>
+	</div>`);
+	$("#createBfAccount").on("click", function() {
+			renderCreateAccountPage();
+	});
+	$(".btn.submitUsrPas").on("click", function(e) {
+		e.preventDefault();
+		renderDashboardPage();
+	})
+}
+
+function renderCreateAccountPage() {
+	// when you click the create account button, it will bring you to the Create Account Screen
+	$("main").html(`
+	<div class="page createAccount">
+		<h1>Create Account Page</h1>
+		<form>
+			Username:<br>
+			<input type="text" class="newUsername" required><br>
+			Password:<br>
+			<input type="password" class ="newPassword" required><br>
+			<input type="submit" value="Create Account">
+			<button class="btn home">Cancel</button>
+			<button class="btn login">Already have an account?</button>
+		</form>
+	</div>`);
+	$(".btn.home").on("click", function() {
+		renderHomePage();
+	});
+	$(".btn.login").on("click", function() {
+		renderLoginPage();
+	})
+}
+
+function renderDashboardPage() {
+	$("main").html(`
+	<div class="page dashboard">
+		<h1>Dashboard Page</h1>
+		<button class="btn createNewFriend">Create New Friend Profile</button>
+		<button class="btn existingFriend">Existing Friend</button>
+		<button class="btn logout">Log Out</button>
+	</div>`);
+	$(".btn.createNewFriend").on("click", function() {
+		renderCreateFriendPage();
+	});
+	$(".btn.existingFriend").on("click", function() {
+		renderFriendProfilePage();
+	});
+	$(".btn.logout").on("click", function() {
+		renderHomePage();
 	});
 }
+ 
+function renderCreateFriendPage() {
+	$("main").html(`
+	<div class="page createFriendProfile">
+		<h1>Create New Friend Page</h1>
+		<form action="">
+			Relationship:<br>
+			<input type="text" required><br>
+			First Name:<br>
+			<input type="text" required><br>
+			Last Name:<br>
+			<input type="text"><br>
+			Birthday:<br>
+			<input type="date" required><br>
+			Email Address:<br>
+			<input type="email"><br>
+			Home Address:<br>
+			<input type="text"><br>
+			Phone Number:<br>
+			<input type="text"><br>
+			<input type="reset">
+			<input type="submit" value="Save">
+			<button class="btn cancelCreateNewFriend">Cancel</button>
+		</form>
+	</div>`);
 
-function createNewFriend() {
-	// on click of "new friend button", the createFriendPage shows
+	$(".btn.cancelCreateNewFriend").on("click", function() {
+		renderDashboardPage();
+	})
 }
 
-function saveNewFriend() {
-	//on click, the information that's in the form gets saved
+function renderEditFriendPage() {
+	$("main").html(`
+	<div class="page editFriendProfile">
+		<h1>Edit Friend's Profile</h1>
+		<form action="">
+				Relationship:<br>
+				<input type="text"><br>
+				First Name:<br>
+				<input type="text"><br>
+				Last Name:<br>
+				<input type="text"><br>
+				Birthday:<br>
+				<input type="date"><br>
+				Email Address:<br>
+				<input type="email"><br>
+				Home Address:<br>
+				<input type="text"><br>
+				Phone Number:<br>
+				<input type="text"><br>
+				<input type="reset">
+				<input type="submit" value="Save">
+			</form>
+			<button class="btn cancelEditNewFriend">Cancel</button>
+	</div>`);
+	$(".btn.cancelEditNewFriend").on("click", function() {
+		renderDashboardPage();
+	})
+
+}
+
+function renderFriendProfilePage() {
+	$("main").html(`
+	<div class="page viewFriendProfile">
+		<h1>Existing Profile Page</h1>
+		<button class="btn editFriend">Edit Friend</button>
+		<button class="btn dashboard">Back to Dashboard</button>
+	</div>
+	`);
+	$(".btn.editFriend").on("click", function() {
+		renderEditFriendPage();
+	});
+	$(".btn.dashboard").on("click", function() {
+		renderDashboardPage();
+	})
 }
 
 
-function viewFriendProfile() {
-	//on click displays the friend's page
-	//calls renderFriendInfo to populate the friend's information
-}
-
-function renderFriendInfo() {
-	// when called, this function will populate the friend's saved dates, wishlist items, and picture
+function initializeApp() {
+	renderHomePage();
 }
 
 
-function viewDashboard() {
-	// on click you're taken to your homescreen/dashboard page
-}
 
-function viewWishList() {
-	// on click the WishListPage shows up and everything else is hidden 
-}
+initializeApp();
 
 
-function toggleHideShow() {
-
-}
-
-
-$(loginButton);
