df6b6d0cc952dc9627d6a0596a1c6b2763a7328c
pauldoubleyouare <paulrosenberg13@gmail.com>
2018-09-25 08:35:40 -0700

    updated put endpoint for users

--- server.js
+++ server.js
@@ -95,12 +95,30 @@ app.post('/users', (req, res) => {
 
 app.put('/users/:id', (req, res) => {
     console.log('PUT request received');
-    res.json(req.params.id);
     //take the req.params.id, query the database to find the user with the matching Id, if it finds a matching id
     //then we'll update the document in our DB with the data that was sent over in the request
     //if we do not find an object matching that ID, then we will throw a response to the client saying we couldn't find it
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message = `You're missing some fields or they don't match. Id in request parameter (${req.params.id}) must match the ID in the body (${req.body.id})`;
+        console.error(message);
+        res.status(400).json({message: message});
+    }
 
-})
+    const fieldsToUpdate = {};
+    const updateableFields = ['userName', 'fullName', 'email'];
+    updateableFields.forEach(field => {
+        if(field in req.body) {
+            fieldsToUpdate[field] = req.body[field];
+        } 
+    });
+
+    User
+        .findByIdAndUpdate(req.params.id, {$set: fieldsToUpdate})
+        .then(user => {
+            console.log(user);
+            return res.status(204).end()})
+        .catch(err => res.status(500).json({message: 'Internal server error'}))
+});
 
 
 // we're calling server up here, then assigning a value to it inside of runServer, but closeServer also needs access to a server object
