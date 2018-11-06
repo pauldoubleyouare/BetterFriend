72a4fe6ebab8d4a60eba0b3db84d5c8750bd5444
pauldoubleyouare <paulrosenberg13@gmail.com>
2018-11-05 20:02:33 -0800

    cleaned up userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,6 @@ const { Profile } = require('../models/profileModel');
 // api/profiles/:id
 // api/users/:id/profiles/:id
 
-
 router.use(
   '/:id/profiles/',
   (req, res, next) => {
@@ -21,7 +20,6 @@ router.use(
   },
   profileRoutes
 );
-// inside of a profile route - GET all profiles of this user /api/users/:id/profiles/
 
 router.get('/', (req, res) => {
   User.find()
@@ -29,7 +27,6 @@ router.get('/', (req, res) => {
       res.json({
         users: users.map(user => user.serialize())
       });
-      // console.log(users);
 
       return users;
     })
@@ -41,17 +38,11 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   let id = req.params.id;
-  // console.log('id', id);
+
   User.findById(id)
     .then(user => {
-      // console.log('user', user)
-      //get all profiles for this user
-      //get wishlist items for the profile
       return Profile.find({ owner: id }).then(profiles => {
-        //put them inside of the User object
         user.profiles = profiles;
-        // console.log(profiles);
-        //return the user
         return user;
       });
     })
@@ -64,32 +55,25 @@ router.get('/:id', (req, res) => {
 
 //*****How would I dig into the fullName object to make sure that firstName and lastName are there, right now throwing a 500  */
 router.post('/', (req, res, next) => {
-  const requiredFields = ['userName', 'fullName', 'password', 'email']; //creating a model for the required fields
-  const missingField = requiredFields.find(field => !(field in req.body)); //check to see if there are missing fields inside of req.body
+  const requiredFields = ['userName', 'fullName', 'password', 'email'];
+  const missingField = requiredFields.find(field => !(field in req.body));
   console.log('MISSING FIELD>>>>>>>>>>>>>>', missingField);
 
   // ===========Login/user credential validation=============//
   if (missingField) {
-    //if there are missing fields
     return res.status(422).json({
       code: 422,
       reason: 'ValidationError',
       message: `Missing field in request body`,
       location: missingField
     });
-    // const err = new Error(`Missing ${missingField} in request body`); //create a new error message
-    // err.status = 422; //send 422 - unprocessable entity
-    // return next(err); //end this middleware function and return the error
   }
 
-  // Comparing a set of fields, that should be strings, to the req.body, and setting those NON string fields as a variable callled nonStringFields
-  // .find() and array method - which returns the value of the first element that's true, given the testing function
   const stringFields = ['userName', 'password', 'email'];
   const nonStringField = stringFields.find(
     field => field in req.body && typeof req.body[field] !== 'string'
   );
 
-  // if we have some nonStringFields (in req.body) then we're creating and returning an error message,
   //******** next() calls the next middleware function, but is that where does this get error message get processed? inside of server.js? ******/
   if (nonStringField) {
     return res.status(422).json({
@@ -98,20 +82,13 @@ router.post('/', (req, res, next) => {
       message: `Incorect field type: expected string`,
       location: nonStringField
     });
-
-    // const err = new Error(`Field: ${nonStringField} must be type String`);
-    // err.status = 422;
-    // return next(err);
   }
 
-  //Specifying fields that we'll be throwing an error for, if they have spaces before or after
-  //If we trim a provided field, and it does NOT equal what's in req.body - we're calling that a non-trimmed field
   const explicitlyTrimmedFields = ['userName', 'password'];
   const nonTrimmedField = explicitlyTrimmedFields.find(
     field => req.body[field].trim() !== req.body[field]
   );
 
-  //If we have some fields that are not trimmed, aka HAVE whitespace, then we're shutting down the rest of the block
   if (nonTrimmedField) {
     return res.status(422).json({
       code: 422,
@@ -119,20 +96,13 @@ router.post('/', (req, res, next) => {
       message: `Cannot start or end with a space`,
       location: nonTrimmedField
     });
-    // const err = new Error(`Field: '${nonTrimmedField}' cannot start or end with a 'space'`);
-    // err.status = 422;
-    // return next(err);
   }
 
-  //Setting the character length requirements of username and password
   const sizedFields = {
     userName: { min: 3 },
     password: { min: 8, max: 72 }
   };
 
-  //Object.keys returns an array, where each element is a string that corresponds to the properties of the object
-  //*****Not 100% sure what this is doing? */
-  //we're checking to see if the fields in req.body (username at least 3 && password 8-72) are too small
   const tooSmallField = Object.keys(sizedFields).find(
     field =>
       'min' in sizedFields[field] &&
@@ -145,7 +115,6 @@ router.post('/', (req, res, next) => {
       req.body[field].trim().length > sizedFields[field].max
   );
 
-  //If there are fields that are too small, then
   if (tooSmallField || tooLargeField) {
     return res.status(422).json({
       code: 422,
@@ -156,18 +125,6 @@ router.post('/', (req, res, next) => {
       location: tooSmallField || tooLargeField
     });
   }
-
-  // const min = sizedFields[tooSmallField].min;
-  // const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
-  // err.status = 422;
-  // return next(err);
-
-  // if (tooLargeField) {
-  //     const max = sizedFields[tooLargeField].max;
-  //     const err = new Error(`Field: '${tooLargeField}' must be less than ${max} characters`);
-  //     err.status = 422;
-  //     return next(err);
-  // }
 
   //****Don't quite understand how fullName will work, since I have it as another aboject fullName: {frstname, lastname} */
   //***** Also don't know what the fullName ="" does/is */
@@ -209,33 +166,6 @@ router.post('/', (req, res, next) => {
       }
       res.status(500).json({ code: 500, message: 'Internal server error' });
     });
-
-  //======ORIGINAL========
-  // User
-  //     .findOne({userName: req.body.userName})
-  //     .then(user => {
-  //         if (user) {
-  //             const message = `${req.body.userName} is already taken, please choose a new username`;
-  //             console.log(message);
-  //             return res.status(400).send(message);
-  //         }
-  //         else {
-  //             User
-  //                 .create({
-  //                     userName: req.body.userName,
-  //                     fullName: {
-  //                         firstName: req.body.fullName.firstName,
-  //                         lastName: req.body.fullName.lastName
-  //                     },
-  //                     email: req.body.email
-  //                 })
-  //                 .then(user => res.status(201).json(user.serialize()));
-  //         }
-  //     })
-  //     .catch(err => {
-  //         console.error(err);
-  //         res.status(500).json({ error: 'something is not right'});
-  //     });
 });
 
 //*****NEED TO ADD/UPDATE PASSWORD */
@@ -269,7 +199,6 @@ router.put('/:id', (req, res) => {
       res.status(500).json({ message: err + 'Internal server error' })
     );
 });
-
 
 //****NEED TO ADD / UPDATE PASSWORD */
 router.delete('/:id', (req, res) => {
